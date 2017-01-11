71066b62c18e1b52a742baf38c22a69e41ecdb2e
Seorenn <hirenn@gmail.com>
2017-01-11 11:41:57 +0900

    Preparation Repository and Project

---
+++ SRSwitch/SRSwitch.h
@@ -0,0 +1,19 @@
+//
+//  SRSwitch.h
+//  SRSwitch
+//
+//  Created by Heeseung Seo on 2017. 1. 11..
+//  Copyright © 2017년 Seorenn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SRSwitch.
+FOUNDATION_EXPORT double SRSwitchVersionNumber;
+
+//! Project version string for SRSwitch.
+FOUNDATION_EXPORT const unsigned char SRSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SRSwitch/PublicHeader.h>
+
+
